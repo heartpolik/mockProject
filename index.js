847e0fd5a76c5e18a51a93e86037a6627f9fa125
popadiuk <andriy.popadiuk@ria.com>
2018-11-15 08:58:02 +0200

    feat(Фотоблок): Вебп формат для фото
    Для свайпу в апі фотоблоку додав формат фото webp

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Фото
+ * @type {{photoBlock: module.exports.photoBlock}}
+ */
+module.exports = {
+  /**
+   * Фото блок
+   * @returns {{photoId: string, photoPathWebp: string, photoPathImg: string}}
+   */
+  photoBlock: function () {
+    return {
+      photoId: '35387',
+      photoPathWebp: 'https://cdn.riastatic.com/photosnewr/auto/new_auto_storage/volkswagen_tiguan__353878-300x200.webp',
+      photoPathImg: 'https://cdn.riastatic.com/photosnewr/auto/new_auto_storage/volkswagen_tiguan__353878-300x200.jpg'
+    }
+  }
+};
